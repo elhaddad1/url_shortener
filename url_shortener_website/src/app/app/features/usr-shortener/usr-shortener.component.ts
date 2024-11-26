382f3c44b4d2490ca73d70da524643f9a160eabc
Ahmed Samir <a.s.elhaddad@gmail.com>
2024-11-26 20:22:12 +0300

    first commit for the website

---
+++ url_shortener_website/src/app/app/features/usr-shortener/usr-shortener.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usr-shortener',
+  standalone: true,
+  imports: [],
+  templateUrl: './usr-shortener.component.html',
+  styleUrl: './usr-shortener.component.scss'
+})
+export class UsrShortenerComponent {
+
+}
